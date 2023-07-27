618a53e1e558e2882c9978abd7fbf68109ec16b8
Damian Ubowski <damian.ubowski.0@gmail.com>
2023-07-27 19:45:01 +0200

    Improve mobile welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,45 +1,38 @@
-import { Flex, Box, Image, Heading, Text, Center } from '@chakra-ui/react'
+import { Flex, Box, Image, Heading, Text, Center, VStack } from '@chakra-ui/react'
 
 const WelcomePage = () => {
-    return <Box h='full'>
-        <Flex h='full'>
-            <Box w={'75%'}>
+    return (
+        <VStack h='full'>
+            <Image
+                h='full'
+                src='background.jpg'
+                fallbackSrc='https://via.placeholder.com/1440x1080'
+                objectFit='cover' />
+            <Center>
                 <Box
-                    id='floating-header'
-                    minW={200} minH={100}
-                    h={'30%'} w={400}>
-                    <Flex
-                        alignContent={'center'}
-                        justifyContent={'center'}
-                        h={'100%'}
-                        position={'relative'}
-                        left={'30%'} top={'100%'}
-                        bg='white'>
-                        <Center>
-                            <Box>
-                                <Heading as='h3' size='lg'>MÓJ URSUS</Heading>
-                                <Text fontSize='lg'>Nasze osiedle, nasza sprawa</Text>
-                                <Text fontSize='sm'>Etapy 1, 2, 3</Text>
-                            </Box>
-                        </Center>
-                        <Box
-                            h={'40%'} w={'3'}
-                            bg='orange'
-                            position={'absolute'}
-                            right={-1} bottom={0}>
-                        </Box>
-                    </Flex>
+                    id='title-box'
+                    position={'relative'}
+                    m={8}
+                    px={{ base: 8, lg: 16 }}
+                    py={8}
+                    _after={{
+                        position: 'absolute',
+                        width: 32,
+                        height: 16,
+                        content: '""',
+                        right: 0,
+                        bottom: 0,
+                        borderRight: '4px solid orange',
+                        borderWidth: {base: 4, lg: 8},
+                        borderColor: 'orange'
+                    }}>
+                    <Heading as='h3' size='lg'>MÓJ URSUS</Heading>
+                    <Text fontSize='lg'>Nasze osiedle, nasza sprawa</Text>
+                    <Text fontSize='sm'>Etapy 1, 2, 3</Text>
                 </Box>
-            </Box>
-            <Box h='full'>
-                <Image
-                    h='full'
-                    src='background.jpg'
-                    fallbackSrc='https://via.placeholder.com/1440x1080'
-                    objectFit='cover'/>
-            </Box>
-        </Flex>
-    </Box>
+            </Center>
+        </VStack>
+    );
 };
 
 export default WelcomePage;
